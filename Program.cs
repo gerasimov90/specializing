ad1c25d3d9eceffcd4480a8bd360bc096933e519
GerasimovVictor <gerasimov.viktor90@gmail.com>
2023-01-29 12:20:12 +0700

    wrong method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿Console.WriteLine("Введите элементы массива через пробел");
 string [] firstArray = Console.ReadLine().Split(" ");
-Console.WriteLine(String.Join("; ", firstArray));
+string [] secondArray = new string[10];
+for (int i = 0; i < firstArray.Length; i++)
+{
+        string element = firstArray[i];
+    if (element.Length < 4)
+    {
+        secondArray[i] = element;
+    }
+}
+Console.WriteLine(String.Join("; ", secondArray));
+
+
