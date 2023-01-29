ee69c5feb1e7293702f8f6dddefc2f1ad3848570
GerasimovVictor <gerasimov.viktor90@gmail.com>
2023-01-29 13:43:29 +0700

    with deleting empty strings

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿Console.WriteLine("Введите элементы массива через пробел");
 string [] firstArray = Console.ReadLine().Split(" ");
-string [] secondArray = new string[10];
+string [] secondArray = new string[firstArray.Length];
 for (int i = 0; i < firstArray.Length; i++)
 {
-        string element = firstArray[i];
+    string element = firstArray[i];
     if (element.Length < 4)
     {
         secondArray[i] = element;
     }
 }
+secondArray = secondArray.Where(x => x != null).ToArray();
 Console.WriteLine(String.Join("; ", secondArray));
 
 
